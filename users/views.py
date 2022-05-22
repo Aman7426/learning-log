47b8775ca653c3b30f1fc5b11806551587d30db5
nxt7 <amanchoudhary7426@gmail.com>
2022-05-22 10:54:03 +0530

    Ready for deployment to heroku.

---
+++ users/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+# Create your views here.
+
+def register(request):
+    """Resitration page for new user"""
+    if request.method != 'POST':
+        #display blank form
+        form = UserCreationForm()
+    else:
+        #process the whole form
+        form = UserCreationForm(data =request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            #log the user in and redirect to home page
+            login(request, new_user)
+            return redirect('learning_logs:index')
+
+    context = {'form':form}
+    return render(request,'registration/register.html',context)
